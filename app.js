ad09cba08ac1708d368c8e979940a6f0b13dce4f
vinayaka0305 <vav6473@gmail.com>
2025-03-09 13:36:59 +0530

    code

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./src/routes/userRoutes')
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 
